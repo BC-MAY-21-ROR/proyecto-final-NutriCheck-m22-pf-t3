2c2dbf4b44982a25500ef7787f9ad5659b05b93e
AgustinPalmaM <palma.agustin7@gmail.com>
2022-07-05 18:55:20 -0400

    Add seeds to test index administrator only show users whit role administrator
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 Speciality.create(name: "administrator")
 Speciality.create(name: "nutriologist")
-User.create(name: "Carlos", last_name: "Salgado", birth_date: "1985-05-20", phone: "+56975654656", email: "carlos.salgado@gmail.com", password: "123456789", role: "admin", speciality_id: 1)
+User.create(name: "Carlos", last_name: "Salgado", birth_date: "1985-05-20", phone: "+56975654656", email: "carlos.salgado@gmail.com", password: "123456789", role: "administrator", speciality_id: 1)
 User.create(name: "Marcelo", last_name: "Delgado", birth_date: "1995-07-12", phone: "+56975666744", email: "marcelo.delgado@gmail.com", password: "123456789", role: "nutriologist", speciality_id: 2)
+User.create(name: "Daniel", last_name: "Gonzalez", birth_date: "2000-01-20", phone: "+56975456787", email: "daniel.gonzalez@gmail.com", password: "123456789", role: "administrator", speciality_id: 1)
