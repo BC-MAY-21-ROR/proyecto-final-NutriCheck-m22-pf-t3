d981511f04387e0b0928d2950a8335338d91f11a
ruruisu <zaddozgl@gmail.com>
2022-07-06 17:52:49 -0500

    added schedule edit and new views and modified controller

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,54 @@
 class SchedulesController < ApplicationController
-end
+
+    def index
+      @schedules = Schedule.all
+    end
+  
+    def new
+      @schedules = Schedule.new
+    end
+    
+    def show
+      @schedule = Schedule.find(params[:id])
+    end
+  
+    def create
+      @schedule = Schedule.new(schedule_params)
+      if @schedule.save
+        notice 'New schedule created'
+      else
+        notice 'ERROR'
+      end
+    end
+  
+    def edit
+      @schedule = Schedule.find(params[:id])
+    end
+  
+    def update
+      @schedule = Schedule.find(params[:id])
+  
+      if @schedule.update(schedule_params)
+        notice 'Schedule modify succesfully'
+      else
+        notice 'ERROR'
+      end
+    end
+  
+    def destroy
+      @schedule = Schedule.find(params[:id])
+      if @schedule.destroy
+        notice 'Schedule deleted succesfully'
+      else
+        notice 'ERROR'
+      end
+    end
+  
+    private
+  
+    def schedule_params
+      params.require(:schedule).permit(:week_day, :opening_time, :closing_time)
+    end
+  
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
- 
   root 'administrators#index'
   resources :administrators
   resources :schedules
