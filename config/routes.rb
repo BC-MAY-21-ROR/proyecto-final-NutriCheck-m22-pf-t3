53c5100edf115f4a6c89f580417ee78f3b7cee2a
AgustinPalmaM <palma.agustin7@gmail.com>
2022-07-08 14:20:07 -0400

    Update routes and add resources for administrators professionals and schedules
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
- 
   root 'users#index'
   resources :users
-
+  resources :managers
+  resources :administrators
+  resources :professionals
+  resources :schedules
 end
