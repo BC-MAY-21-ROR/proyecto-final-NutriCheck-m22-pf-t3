bdd6ac2e30f2c4124900df4a1fc4a92f8e02516c
OwenLobato <owenlobatov@gmail.com>
2022-07-11 13:18:43 -0500

    Add methods for specialities & licences controller

--- app/controllers/licenses_controller.rb
+++ app/controllers/licenses_controller.rb
@@ -1,2 +1,53 @@
+# Controller for LICENSES
+
 class LicensesController < ApplicationController
+  def index
+    @licences = License.all
+  end
+
+  def new
+    @licence = License.new
+  end
+
+  def show
+    @licence = License.find(params[:id])
+  end
+
+  def create
+    @licence = License.new(lic_params)
+    if @licence.save
+      redirect_to licences_path, notice: 'New licence created successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @licence = License.find(params[:id])
+  end
+
+  def update
+    @licence = License.find(params[:id])
+
+    if @licence.update(lic_params)
+      redirect_to licences_path, notice: 'License was edited successfully'
+    else
+      render :edit, :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @licence = License.find(params[:id])
+    if @licence.destroy
+      redirect_to licences_path, notice: 'License was deleted successfully'
+    else
+      render :edit, :unprocessable_entity
+    end
+  end
+
+  private
+
+  def lic_params
+    params.require(:licence).permit(:tyoe, :number, :user_id)
+  end
 end
--- app/controllers/specialities_controller.rb
+++ app/controllers/specialities_controller.rb
@@ -1,2 +1,53 @@
+# Controller for SPECIALITIES
+
 class SpecialitiesController < ApplicationController
+  def index
+    @specialities = Speciality.all
+  end
+
+  def new
+    @speciality = Speciality.new
+  end
+
+  def show
+    @speciality = Speciality.find(params[:id])
+  end
+
+  def create
+    @speciality = Speciality.new(speci_params)
+    if @speciality.save
+      redirect_to specialities_path, notice: 'New speciality created successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @speciality = Speciality.find(params[:id])
+  end
+
+  def update
+    @speciality = Speciality.find(params[:id])
+
+    if @speciality.update(speci_params)
+      redirect_to specialities_path, notice: 'Speciality was edited successfully'
+    else
+      render :edit, :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @speciality = Speciality.find(params[:id])
+    if @speciality.destroy
+      redirect_to specialities_path, notice: 'Speciality was deleted successfully'
+    else
+      render :edit, :unprocessable_entity
+    end
+  end
+
+  private
+
+  def speci_params
+    params.require(:speciality).permit(:name)
+  end
 end
