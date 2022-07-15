0cae42b126813ea60a967637840408fa686cc847
ruruisu <zaddozgl@gmail.com>
2022-07-14 19:11:52 -0500

    fixed appointment controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,49 +1,45 @@
 # frozen_string_literal: true
 
+# Appointment class controller
 class AppointmentsController < ApplicationController
-    def index
-      @appointments = Appointment.all
-    end
-  
-    def new
-      @appointment = Appointment.new
-    end
-  
-    def show
-      @appointment = Appointment.find(params[:id])
-    end
-  
-    def create
-      @appointment = Appointment.new(appointment_params)
-      @appointment.save
-      redirect_to appointments_path if @appointment.save
-    end
-  
-    def edit
-      @appointment = Appointment.find(params[:id])
-    end
-  
-    def update
-      @appointment = Appointment.find(params[:id])
-  
-      if @appointment.update(appointment_params)
-  
-      end
-    end
-  
-    def destroy
-      @appointment = Appointment.find(params[:id])
-      if @appointment.destroy
-        notice 'Appointment deleted succesfully'
-      else
-        notice 'ERROR'
-      end
-    end
-  
-    private
-  
-    def appointment_params
-      params.require(:appointment).permit(:date_time, :reason)
+  def index
+    @appointments = Appointment.all
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.save
+    redirect_to appointments_path if @appointment.save
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    if @appointment.destroy
+      notice 'Appointment deleted succesfully'
+    else
+      notice 'ERROR'
     end
   end
-  
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:date_time, :reason, :service_id, :user_id)
+  end
+end
