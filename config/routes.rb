d121e6d4d081c6ccd75a8d805b0b627b71c2301e
AgustinPalmaM <palma.agustin7@gmail.com>
2022-07-15 15:46:10 -0400

    Created routes for managers_admin and in_contruction
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   resources :patients
   resources :cards
   resources :services
+  get 'managers_admin', to: 'managers#admin'
+  get 'in_construction', to: 'managers#in_construction'
 end
