98d854424bae721e0eb5283232faada342748e30
ruruisu <zaddozgl@gmail.com>
2022-07-26 15:13:09 -0500

    removed unused code

--- db/migrate/20220719204812_add_devise_to_patients.rb
+++ db/migrate/20220719204812_add_devise_to_patients.rb
@@ -13,38 +13,16 @@ class AddDeviseToPatients < ActiveRecord::Migration[7.0]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.string   :current_sign_in_ip
-      # t.string   :last_sign_in_ip
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
-
-      # Uncomment below if timestamps were not included in your original model.
-      # t.timestamps null: false
     end
 
 
     add_index :patients, :reset_password_token, unique: true
-    # add_index :patients, :confirmation_token,   unique: true
-    # add_index :patients, :unlock_token,         unique: true
+
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
+
     raise ActiveRecord::IrreversibleMigration
   end
 end
