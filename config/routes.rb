56bc7297c80bda2847367477d767b3c91e4657f1
AgustinPalmaM <palma.agustin7@gmail.com>
2022-07-27 20:23:37 -0400

    Complete some configuration over devise for patients and users model, because it was necessary to continue developing patient dashboard and its features like appointments coming and historic appointments
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
+  
   devise_for :users
-  devise_for :patients
+  devise_for :patients, controllers: {
+    registrations: 'patients/registrations'
+  }
   devise_for :administrators
 
   resources :slots
