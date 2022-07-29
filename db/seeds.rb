9cc9e7bcb4abca73c42617a0cac23ffb2707b746
AgustinPalmaM <palma.agustin7@gmail.com>
2022-07-29 02:34:50 -0400

    Create seeds to simulate patients and appointments from 01-01-2022 until 30-07-2022
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,6 @@
 #   Character.create(name: "Luke", movie: movies.first)
 # Speciality.create(name: 'administrator')
 # Speciality.create(name: 'nutriologist')
-# User.create(name: 'Carlos', last_name: 'Salgado', birth_date: '1985-05-20', phone: '+56975654656',
-#             email: 'carlos.salgado@gmail.com', password: '123456789', role: 'administrator', speciality_id: 1)
-# User.create(name: 'Marcelo', last_name: 'Delgado', birth_date: '1995-07-12', phone: '+56975666744',
-#             email: 'marcelo.delgado@gmail.com', password: '123456789', role: 'nutriologist', speciality_id: 2)
-# User.create(name: 'Daniel', last_name: 'Gonzalez', birth_date: '2000-01-20', phone: '+56975456787',
-#             email: 'daniel.gonzalez@gmail.com', password: '123456789', role: 'administrator', speciality_id: 1)
-
-
 
 # i = 9
 # while i < 18
@@ -76,7 +68,6 @@
 #   i+=1
 # end
 
-
 #   Slot.create(service: 3, professional: 34, start_time: "2022-07-25 10:00:00 UTC", status: 'not available')
 #   Slot.create(service: 3, professional: 34, start_time: "2022-07-25 11:00:00 UTC", status: 'not available')
 #   Slot.create(service: 3, professional: 34, start_time: "2022-07-25 12:00:00 UTC", status: 'not available')
@@ -86,8 +77,7 @@
 #   Slot.create(service: 3, professional: 34, start_time: "2022-07-25 16:00:00 UTC", status: 'not available')
 #   Slot.create(service: 3, professional: 34, start_time: "2022-07-25 17:00:00 UTC", status: 'not available')
 
-
-# 5.times do 
+# 5.times do
 #   Slot.create(start_time: "2022-08-01 09:00:00 UTC")
 #   Slot.create(start_time: "2022-08-01 10:00:00 UTC")
 #   Slot.create(start_time: "2022-08-01 11:00:00 UTC")
@@ -99,7 +89,7 @@
 #   Slot.create(start_time: "2022-08-01 17:00:00 UTC")
 # end
 
-# 5.times do 
+# 5.times do
 #   Slot.create(start_time: "2022-08-02 09:00:00 UTC")
 #   Slot.create(start_time: "2022-08-02 10:00:00 UTC")
 #   Slot.create(start_time: "2022-08-02 11:00:00 UTC")
@@ -111,7 +101,7 @@
 #   Slot.create(start_time: "2022-08-02 17:00:00 UTC")
 # end
 
-# 5.times do 
+# 5.times do
 #   Slot.create(start_time: "2022-08-03 09:00:00 UTC")
 #   Slot.create(start_time: "2022-08-03 10:00:00 UTC")
 #   Slot.create(start_time: "2022-08-03 11:00:00 UTC")
@@ -123,7 +113,7 @@
 #   Slot.create(start_time: "2022-08-03 17:00:00 UTC")
 # end
 
-# 5.times do 
+# 5.times do
 #   Slot.create(start_time: "2022-08-04 09:00:00 UTC")
 #   Slot.create(start_time: "2022-08-04 10:00:00 UTC")
 #   Slot.create(start_time: "2022-08-04 11:00:00 UTC")
@@ -135,7 +125,7 @@
 #   Slot.create(start_time: "2022-08-04 17:00:00 UTC")
 # end
 
-# 5.times do 
+# 5.times do
 #   Slot.create(start_time: "2022-08-05 09:00:00 UTC")
 #   Slot.create(start_time: "2022-08-05 10:00:00 UTC")
 #   Slot.create(start_time: "2022-08-05 11:00:00 UTC")
@@ -147,7 +137,7 @@
 #   Slot.create(start_time: "2022-08-05 17:00:00 UTC")
 # end
 
-# 5.times do 
+# 5.times do
 #   Slot.create(start_time: "2022-08-06 09:00:00 UTC")
 #   Slot.create(start_time: "2022-08-06 10:00:00 UTC")
 #   Slot.create(start_time: "2022-08-06 11:00:00 UTC")
@@ -159,49 +149,166 @@
 #   Slot.create(start_time: "2022-08-06 17:00:00 UTC")
 # end
 
+# Run seed to create Patient, can create women and men
+# @first_name_man = %w[Adolfo	Adrián	Agustín	Alberto	Alejandro	Alexander	Alexis	Alonso	Andrés	Angel	Anthony	Antonio	Bautista	Benicio	Benjamín	Carlos	Cesar	César	Cristóbal	Daniel	David	Diego	Dylan	Eduardo	Emiliano	Emilio	Emmanuel	Enrique	Erik	Ernesto	Esteban	Ethan	Evaristo	Fabián	Facundo	Felipe	Félix	Fernando	Francisco	Gabriel	Gaspar	Geronimo	Giorgio	Gregorio	Gustavo	Hugo	Ian	Ignacio	Iker	Isaac	Jacob	Javier	Jayden	Jeremy	Jerónimo	Jesus	Jesús	Joaquín	John	Jorge	Jose	José	Josué	Juan	Julián	Julio	Justin	Kevin	Lautaro	Liam	Lian	Lorenzo	Lucas	Luciano	Luis	Manuel	Marco	María	Mario	Martin	Martín	Mateo	Matías	Maximiliano	Máximo	Maykel	Melquiades	Miguel	Nelson	Noah	Oscar	Pablo	Pedro	Rafael	Ramón	Raúl	Ricardo	Rigoberto	Roberto	Rolando	Ronny	Samuel	Santiago	Santino	Santos	Sebastián	Sergio	Thiago	Tomás	Valentino	Vicente]
+# @second_name_man = %w[Victor	Víctor	Acton	Adam	Aiken	Alein	Alan	Andrew	Arnold	Austin	Baldwin	Barden	Barnett	Barton	Basil	Bishop	Bolton	Brent	Bruce	Caden	Carlton	Carter	Casper	Chad	Chase	Clark	Clint	Cole	Conrad	Cooper	Curtis	Cypril	Dawson	Dean	Denis	Dominic	Donald	Drake	Dunstan	Eban	Edgar	Edwin	Elton	Emil	Eton	Ferdinand	Francis	Fraser	Frederick	Fulbert	Gary	Gilbert	Giles	Gray	Guy	Hanson	Harald	Harrison	Harry	Henniker	Herbert	Homer	Hugh	Indiana	Jaden	James	Jasper	Jett	Jordan	Joshua	Jude	Kurtis	Lauren	Leighton	Leonard	Leonel	Lewis	Lionel	Locke	Luke	Mark	Marlon	Morris	Moses	Nick	Paris	Patrick	Pierce	Raven	Ray	Rayner	River	Roger	Scott	Stuart	Sutton	Taylor	Terence	Terry	Trent	Ulric	Usher	Vincent	Walt	Walter	Warden	Weldon	Wes	Wesley	Wyatt	Zayden]
 
-@first_name_man = %w[Pedro Evaristo Melquiades Ronny John Maximiliano Joaquín Matías Martín José Luciano Jorge Miguel Carlos Facundo Julián Gabriel Máximo Jose Juan Luis Francisco Jesus]
-@second_name_man = %w[Mateo Agustín Santiago Tomás Benjamín Lucas Gaspar Mario Geronimo Ernesto Martin Giorgio Ricardo Fernando Javier Sergio Roberto Manuel Victor Antonio Alejandro Alonso Vicente]
-@last_name = %w[Hernandez Garcia Martinez Lopez Gonzalez Perez Rodriguez Sanchez Ramirez Cruz Flores Gomez Morales Vazquez Reyes]
-@second_last_name = %w[Jimenez Torres Diaz Gutierrez Ruiz Mendoza Aguilar Ortiz Moreno Castillo Romero Alvarez Mendez Chavez Rivera]
-@i = 10000
-while(@i <= 10500) do 
-  first_name = @first_name_man[rand(19)]
-  second_name = @second_name_man[rand(19)]
-  last_name = @last_name[rand(15)]
-  second_last_name = @second_last_name[rand(15)]
-  Patient.create( id: @i,
-                  name: first_name,
-                  second_name: second_name,
-                  last_name: last_name, 
-                  second_last_name: second_last_name,
-                  birth_date: "#{rand(1962..2015)}-#{rand(1..13)}-#{rand(1..29)} 17:00:00 UTC", 
-                  phone: "#{rand(900000000..999999999)}", 
-                  email: "#{first_name}.#{last_name}.#{second_last_name}@gmail.com",
-                  password: "123456", 
-                  height: rand(110..200).to_f, observations: "Creation Patient")
-  @i+=1
-end
-
-
-# @first_name_woman = %w[Catalina Enriqueta Marlene Siomara Nariana Denisse Patricia Pilar Geovana Olivia Marta Roxana Sonia Agustina Emilia Isidora Florencia Luciana Salomé Isabella Mariana Gabriela Emily]
-# @second_name_woman = %w[Danna Mía Domenica Elizabeth Beatriz Ramona Liz Ariana Briana María Julieta Martina Emma Camila Sofía Valentina Victoria Maria Ana Rosa Juana Mar Guadalupe]
-# @i = 601
-# while(@i <= 700) do 
-#   first_name = @first_name_woman[rand(19)]
-#   second_name = @second_name_woman[rand(19)]
+# @last_name = %w[Acosta	Aguilar	Aguirre	Alonso	Alvarado	Alvarez	Andrade	Angeles	Antonio	Arellano	Arias	Arroyo	Avalos	Avila	Ayala	Barajas	Barrera	Bautista	Becerra	Beltrán	Benítez	Bernal	Bravo	Cabrera	Calderón	Camacho	Campos	Cano	Cárdenas	Carmona	Carrillo	Castañeda	Castellanos	Castillo	Castro	Cervantes	Chan	Chávez	Cisneros	Contreras	Córdova	Corona	Cortés	Cortez	Cruz	Cuevas	Dávila	Delgado	Diaz	Domínguez	Durán	Enríquez	Escobar	Esparza	Espinosa	Espinoza	Esquivel	Estrada	Félix	Fernández	Figueroa	Flores	Franco	Fuentes	Galindo	Gallardo	Gallegos	Galván	García	Garza	Gómez	González	Guerra	Guerrero	Guevara	Gutiérrez	Guzmán	Hernández	Herrera	Huerta	Ibarra	Jiménez	Juárez	Lara	Leon	Leyva	López	Lozano	Lugo	Luna	Macías	Magaña	Maldonado	Marín	Márquez	Martínez	Mata	Medina	Mejía	Méndez	Mendoza	Mercado	Meza	Miranda	Molina	Montes	Montiel	Montoya	Mora	Morales	Moreno	Muñoz	Murillo	Nava	Navarrete	Navarro	Nuñez	Ochoa	Olivares	Olvera	Orozco	Ortega	Ortiz	Osorio	Pacheco	Padilla	Palacios	Paredes	Parra	Peña	Peralta	Pérez	Pineda	Ponce	Quintero	Quiroz	Ramírez	Ramos	Rangel	Reséndiz	Reyes	Reyna	Ríos	Rivas	Rivera	Robles	Rocha	Rodríguez	Rojas	Román	Romero	Rosales	Rosas	Rubio	Ruiz	Salas	Salazar	Salgado	Salinas	Sánchez	Sandoval	Santiago	Santos	Saucedo	Segura	Serrano	Silva	Solis	Sosa	Soto	Suarez	Tapia	Téllez	Torres	Tovar	Trejo	Trujillo	Valdez	Valencia	Valenzuela	Vargas	Vásquez	Vázquez	Vega	Velasco	Velázquez	Vera	Villa	Villalobos	Villanueva	Villarreal	Villegas	Zamora	Zárate	Zavala	Zúñiga]
+# @second_last_name = %w[Acosta	Aguilar	Aguirre	Alonso	Alvarado	Alvarez	Andrade	Angeles	Antonio	Arellano	Arias	Arroyo	Avalos	Avila	Ayala	Barajas	Barrera	Bautista	Becerra	Beltrán	Benítez	Bernal	Bravo	Cabrera	Calderón	Camacho	Campos	Cano	Cárdenas	Carmona	Carrillo	Castañeda	Castellanos	Castillo	Castro	Cervantes	Chan	Chávez	Cisneros	Contreras	Córdova	Corona	Cortés	Cortez	Cruz	Cuevas	Dávila	Delgado	Diaz	Domínguez	Durán	Enríquez	Escobar	Esparza	Espinosa	Espinoza	Esquivel	Estrada	Félix	Fernández	Figueroa	Flores	Franco	Fuentes	Galindo	Gallardo	Gallegos	Galván	García	Garza	Gómez	González	Guerra	Guerrero	Guevara	Gutiérrez	Guzmán	Hernández	Herrera	Huerta	Ibarra	Jiménez	Juárez	Lara	Leon	Leyva	López	Lozano	Lugo	Luna	Macías	Magaña	Maldonado	Marín	Márquez	Martínez	Mata	Medina	Mejía	Méndez	Mendoza	Mercado	Meza	Miranda	Molina	Montes	Montiel	Montoya	Mora	Morales	Moreno	Muñoz	Murillo	Nava	Navarrete	Navarro	Nuñez	Ochoa	Olivares	Olvera	Orozco	Ortega	Ortiz	Osorio	Pacheco	Padilla	Palacios	Paredes	Parra	Peña	Peralta	Pérez	Pineda	Ponce	Quintero	Quiroz	Ramírez	Ramos	Rangel	Reséndiz	Reyes	Reyna	Ríos	Rivas	Rivera	Robles	Rocha	Rodríguez	Rojas	Román	Romero	Rosales	Rosas	Rubio	Ruiz	Salas	Salazar	Salgado	Salinas	Sánchez	Sandoval	Santiago	Santos	Saucedo	Segura	Serrano	Silva	Solis	Sosa	Soto	Suarez	Tapia	Téllez	Torres	Tovar	Trejo	Trujillo	Valdez	Valencia	Valenzuela	Vargas	Vásquez	Vázquez	Vega Velasco	Velázquez	Vera	Villa	Villalobos	Villanueva	Villarreal	Villegas	Zamora	Zárate	Zavala	Zúñiga]
+# @i = 1
+# while(@i <= 5000) do
+#   first_name = @first_name_man[rand(19)]
+#   second_name = @second_name_man[rand(19)]
 #   last_name = @last_name[rand(15)]
 #   second_last_name = @second_last_name[rand(15)]
 #   Patient.create( id: @i,
 #                   name: first_name,
 #                   second_name: second_name,
-#                   last_name: last_name, 
+#                   last_name: last_name,
 #                   second_last_name: second_last_name,
-#                   birth_date: "#{rand(1962..2015)}-#{rand(1..13)}-#{rand(1..29)} 17:00:00 UTC", 
-#                   phone: "#{rand(900000000..999999999)}", 
+#                   birth_date: "#{rand(1962..2015)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#                   phone: "#{rand(900000000..999999999)}",
 #                   email: "#{first_name}.#{last_name}.#{second_last_name}@gmail.com",
-#                   password: "123456", 
+#                   password: "123456",
 #                   height: rand(110..200).to_f, observations: "Creation Patient")
 #   @i+=1
 # end
 
+# @first_name_woman = %w[Enriqueta Marlene Siomara Nariana Denisse Patricia Pilar Geovana Olivia Marta Roxana Sonia Luciana Salomé Isabella Mariana Gabriela Emily Abigail Abril Agustina Alaia Alana Alba Luz Alejandra Alicia Alma Altagracia Amala Amalia Amanda Ana Andrea Angeles Antonella Antonia Ariana Ashley Bárbara Beatriz Belén Briana Brithany Camila Caridad Carmen Carolina Catalina Celeste Claudia Concepción Daniela Danna Dayana Delfina Domenica Dulce Elizabeth Emma Esmeralda Evelyn Florencia Francesca Francisca Génesis Guadalupe Isidora Jimena Josefa Josefina Juana Julia Julieta Kamila Karla Kiara]
+# @second_name_woman = %w[Danna Mía Domenica Elizabeth Beatriz Ramona Liz Ariana Briana María Julieta Martina Emma Camila Sofía Valentina Victoria Maria Ana Rosa Juana Mar Guadalupe Kimberly Laura Leticia Lidia Lisandra Liz Luana Lucía Luciana Luisa Fernanda Luz Luz María Mabel Maite Margarita María Mariangel Mariapaz Maritza Martha Martina Mayelin Melany Mercedes Mia Mikaela Morena Noemí Odalys Olga Olivia Patricia Paula Ramona Raquel Reina Isabel Rosa Rosmery Roxana Salomé Samantha Sara Silvia Sofía Sonia Sophia Tania Teresa Trinidad Valentina Valeria Vanessa Verónica Victoria Ximena Yadira Yaima Yamileth Yanelis Yanet Zoe]
+# @last_name = %w[Hernandez Garcia Martinez Lopez Gonzalez Perez Rodriguez Sanchez Ramirez Cruz Flores Gomez Morales Vazquez Reyes Jimenez Torres Diaz Gutierrez Ruiz Mendoza Aguilar Ortiz Moreno Castillo Romero Alvarez Mendez Chavez Rivera]
+# @second_last_name = %w[Hernandez Garcia Martinez Lopez Gonzalez Perez Rodriguez Sanchez Ramirez Cruz Flores Gomez Morales Vazquez Reyes Jimenez Torres Diaz Gutierrez Ruiz Mendoza Aguilar Ortiz Moreno Castillo Romero Alvarez Mendez Chavez Rivera]
+
+
+# @i = 20000
+# while(@i <= 23000) do
+#   first_name = @first_name_woman[rand(78)]
+#   second_name = @second_name_woman[rand(88)]
+#   last_name = @last_name[rand(31)]
+#   second_last_name = @second_last_name[rand(31)]
+#   Patient.create( id: @i,
+#                   name: first_name,
+#                   second_name: second_name,
+#                   last_name: last_name,
+#                   second_last_name: second_last_name,
+#                   birth_date: "#{rand(1962..2015)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#                   phone: "#{rand(900000000..999999999)}",
+#                   email: "#{first_name}.#{last_name}.#{second_last_name}@gmail.com",
+#                   password: "123456",
+#                   height: rand(110..200).to_f, observations: "Creation Patient")
+#   @i+=1
+# end
+
+# Create User team for the company
+# User.create(id: 1,
+# 						name: 'Javier Andres',
+#             last_name: 'Bellarta Sotelo',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975654656',
+#             email: 'Javier.bellarta@nutricheck.com',
+#             password: '123456',
+#             role: 'administrator',
+#             speciality_id: 1)
+
+# User.create(id: 2,
+# 						name: 'Carlos Daniel',
+#             last_name: 'Salgado Moreno',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975654656',
+#             email: 'carlos.salgado@nutricheck.com',
+#             password: '123456',
+#             role: 'manager',
+#             speciality_id: 1)
+
+# User.create(id: 3,
+# 						name: 'Marcelo Cristobal',
+#             last_name: 'Delgado Hermosilla',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975666744',
+#             email: 'marcelo.delgado@nutricheck.com',
+#             password: '123456',
+#             role: 'manager',
+#             speciality_id: 1)
+
+# User.create(id: 4,
+# 						name: 'Susana Veronica',
+#             last_name: 'Bravo Santander',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975456787',
+#             email: 'susana.bravo@nutricheck.com',
+#             password: '123456',
+#             role: 'professional',
+#             speciality_id: 2)
+
+# User.create(id: 5, 
+# 						name: 'Carla Macarena',
+#             last_name: 'Mateluna Santis',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975456787',
+#             email: 'carla.mateluna@nutricheck.com',
+#             password: '123456',
+#             role: 'professional',
+#             speciality_id: 3)
+
+# User.create(id: 6,
+# 						name: 'Oscar Alejandro',
+#             last_name: 'Cerda Orostica',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975456787',
+#             email: 'oscar.carda@nutricheck.com',
+#             password: '123456',
+#             role: 'professional',
+#             speciality_id: 2)
+
+# User.create(id: 7,
+# 						name: 'Mariano Hernan',
+#             last_name: 'Villegas Solari',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975456787',
+#             email: 'mariano.villegas@nutricheck.com',
+#             password: '123456',
+#             role: 'professional',
+#             speciality_id: 3)
+
+# User.create(id: 8,
+# 						name: 'Amelia Josefina',
+#             last_name: 'Zapata Soteldo',
+#             birth_date: "#{rand(1977..2002)}-#{rand(1..12)}-#{rand(1..28)} 17:00:00 UTC",
+#             phone: '+56975456787',
+#             email: 'amelia.zapata@nutricheck.com',
+#             password: '123456',
+#             role: 'professional',
+#             speciality_id: 4)
+
+# start_date = Date.new(2022, 02, 01)
+# end_date = Date.new(2022, 07, 30)
+# while (start_date <= end_date) do 
+# 	i = 9
+# 	if (start_date.strftime("%A") != 'Sunday')
+# 		if (i < 18)
+# 			while (i < 18) do
+# 				Appointment.create(date_time: "#{start_date} #{i}:00:00 UTC", reason: 'Consulta por sobrepeso', status: 'open', payment_status: false, score: nil, review: nil, service_id: 1, user_id: 4, patient_id: rand(5929) )
+# 				Appointment.create(date_time: "#{start_date} #{i}:00:00 UTC", reason: 'Consulta por sobrepeso', status: 'open', payment_status: false, score: nil, review: nil, service_id: 2, user_id: 5, patient_id: rand(5929) )
+# 				Appointment.create(date_time: "#{start_date} #{i}:00:00 UTC", reason: 'Consulta por sobrepeso', status: 'open', payment_status: false, score: nil, review: nil, service_id: 5, user_id: 6, patient_id: rand(5929) )
+# 				Appointment.create(date_time: "#{start_date} #{i}:00:00 UTC", reason: 'Consulta por sobrepeso', status: 'open', payment_status: false, score: nil, review: nil, service_id: 4, user_id: 7, patient_id: rand(5929) )
+# 				Appointment.create(date_time: "#{start_date} #{i}:00:00 UTC", reason: 'Consulta por sobrepeso', status: 'open', payment_status: false, score: nil, review: nil, service_id: 3, user_id: 8, patient_id: rand(5929) )
+# 				i+=1
+# 			end
+# 		else
+# 			i = 9
+# 		end
+# 	end
+# 	start_date += 1
+# end
+
+ 
+# 1	"Complementary Therapies"	"1"	30	"2022-07-25 06:03:29.628722"	"2022-07-25 06:04:31.173432"
+# 2	"Reducing Massage"	"1"	40	"2022-07-18 18:08:59.493388"	"2022-07-18 18:08:59.493388"
+# 3	"Physical Trainer Consultation"	"1"	25	"2022-07-18 18:09:30.973541"	"2022-07-18 18:09:30.973541"
+# 4	"Lymphatic Massages"	"1"	40	"2022-07-18 18:10:53.756907"	"2022-07-18 18:10:53.756907"
+# 5	"Consulting Diet Specialist"	"1"	35	"2022-07-18 18:11:55.101704"	"2022-07-18 18:11:55.101704"
+# 4	"Susana Veronica"	"Bravo Santander"	"2002-02-14"	"+56975456787"	"susana.bravo@nutricheck.com"		"professional"
+# 5	"Carla Macarena"	"Mateluna Santis"	"1979-04-07"	"+56975456787"	"carla.mateluna@nutricheck.com"		"professional"
+# 6	"Oscar Alejandro"	"Cerda Orostica"	"1988-12-13"	"+56975456787"	"oscar.carda@nutricheck.com"		"professional"
+# 7	"Mariano Hernan"	"Villegas Solari"	"2001-03-20"	"+56975456787"	"mariano.villegas@nutricheck.com"		"professional"
+# 8	"Amelia Josefina"	"Zapata Soteldo"	"1979-12-17"	"+56975456787"	"amelia.zapata@nutricheck.com"		"professional"
