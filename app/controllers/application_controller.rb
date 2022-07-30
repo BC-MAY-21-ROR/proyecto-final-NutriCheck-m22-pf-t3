2e3c23e5fae4308971eea7da68ceb2c900a88744
ruruisu <zaddozgl@gmail.com>
2022-07-30 17:23:17 -0500

    fixed app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
 
   def devise_parameter_sanitizer
