433f6beb4a83df7bbc2a962efbb879abda53796e
OwenLobato <owenlobatov@gmail.com>
2022-08-04 17:27:29 -0500

    Add seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_07_28_003132) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,8 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_003132) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-
-
   end
 
   create_table "appointments", force: :cascade do |t|
@@ -100,7 +96,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_28_003132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "patients", force: :cascade do |t|
     t.string "name"
