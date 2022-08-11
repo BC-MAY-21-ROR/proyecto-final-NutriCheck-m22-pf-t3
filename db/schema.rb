529a4d9a6411d278d904d2b7c4b734da0b4439ab
AgustinPalmaM <palma.agustin7@gmail.com>
2022-08-11 16:44:21 -0400

    Update easy_calendar config to show sundays in week_calendar
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -73,6 +73,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_220753) do
     t.index ["patient_id"], name: "index_cards_on_patient_id"
   end
 
+  create_table "cars", force: :cascade do |t|
+    t.text "fecha"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "diets", force: :cascade do |t|
     t.string "name", null: false
     t.string "description", null: false
@@ -88,13 +94,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_220753) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_licenses_on_user_id"
-  end
-
-  create_table "meetings", force: :cascade do |t|
-    t.string "name"
-    t.datetime "start_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "patients", force: :cascade do |t|
