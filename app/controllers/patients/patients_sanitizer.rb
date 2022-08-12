fa6e5432f1fdaf8c0bf841325bc86053a5865733
OwenLobato <owenlobatov@gmail.com>
2022-08-12 18:57:23 -0500

    Fix Professional

--- app/controllers/patients/patients_sanitizer.rb
+++ app/controllers/patients/patients_sanitizer.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-
-class PatientsSanitizer < Devise::ParameterSanitizer
-  def initialize(*)
-    super
-    permit(:sign_up, keys: %i[email name second_name last_name second_last_name birth_date phone height observations photo]) 
-    permit(:account_update, keys: %i[email name second_name last_name second_last_name birth_date phone height photo current_password observations photo])
+module Patients
+  class PatientsSanitizer < Devise::ParameterSanitizer
+    def initialize(*)
+      super
+      permit(:sign_up, keys: %i[email name second_name last_name second_last_name birth_date phone height observations photo]) 
+      permit(:account_update, keys: %i[email name second_name last_name second_last_name birth_date phone height photo current_password observations photo])
+    end
   end
 end
