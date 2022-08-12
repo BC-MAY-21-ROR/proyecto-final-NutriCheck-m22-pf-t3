16808cfc3a556b071a344c70eef14ab4874931bf
AgustinPalmaM <palma.agustin7@gmail.com>
2022-08-11 22:46:27 -0400

    Create helper to show days of week availables in a month
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- app/helpers/slots_helper.rb
+++ app/helpers/slots_helper.rb
@@ -1,4 +1,47 @@
 # frozen_string_literal: true
 
 module SlotsHelper
+  @slots_availables = Slot.where(status: 'available')
+
+  def monday_availables
+    @slots_availables.map do |x|
+      x.start_time.strftime('%A %d') if x.start_time.monday?
+    end.reject(&:nil?).uniq.count
+  end
+
+  def tuesday_availables
+    @slots_availables.map do |x|
+      x.start_time.strftime('%A %d') if x.start_time.tuesday?
+    end.reject(&:nil?).uniq.count
+  end
+
+  def wednesday_availables
+    @slots_availables.map do |x|
+      x.start_time.strftime('%A %d') if x.start_time.wednesday?
+    end.reject(&:nil?).uniq.count
+  end
+
+  def thursday_availables
+    @slots_availables.map do |x|
+      x.start_time.strftime('%A %d') if x.start_time.thursday?
+    end.reject(&:nil?).uniq.count
+  end
+
+  def friday_availables
+    @slots_availables.map do |x|
+      x.start_time.strftime('%A %d') if x.start_time.friday?
+    end.reject(&:nil?).uniq.count
+  end
+
+  def saturday_availables
+    @slots_availables.map do |x|
+      x.start_time.strftime('%A %d') if x.start_time.saturday?
+    end.reject(&:nil?).uniq.count
+  end
+
+  def sunday_availables
+    @slots_availables.map do |x|
+      x.start_time.strftime('%A %d') if x.start_time.sunday?
+    end.reject(&:nil?).uniq.count
+  end
 end
