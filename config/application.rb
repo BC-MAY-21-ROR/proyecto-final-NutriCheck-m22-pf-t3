0289ab60b2600f478811d7d2cb5e131a55447439
OwenLobato <owenlobatov@gmail.com>
2022-08-16 17:20:51 -0500

    Add JS function to project

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Nutricheck
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
