1ef899bd77e132cd4a291ea238a95809080521fc
ruruisu <zaddozgl@gmail.com>
2022-08-19 11:15:06 -0500

    fixed user edit

--- app/controllers/users/users_sanitizer.rb
+++ app/controllers/users/users_sanitizer.rb
@@ -4,7 +4,7 @@ module Users
   class UsersSanitizer < Devise::ParameterSanitizer
     def initialize(*)
       super
-      permit(:sign_up, keys: %i[email name last_name birth_date phone role speciality_id])
+      permit(:sign_up, keys: %i[name last_name birth_date phone email speciality_id role])
     end
   end
 end
