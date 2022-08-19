9ab3610e0d7e81532ae2b84c74019050b6c373d6
AgustinPalmaM <palma.agustin7@gmail.com>
2022-08-19 19:25:07 -0400

    Schema is changed after two migrations to make nullable some dates in cards
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_230323) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_19_230738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_230323) do
     t.datetime "date_time", null: false
     t.string "reason", null: false
     t.string "status", null: false
-    t.boolean "payment_status", null: false
+    t.string "payment_status", null: false
     t.integer "score"
     t.string "review"
     t.datetime "created_at", null: false
@@ -61,13 +61,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_230323) do
   end
 
   create_table "cards", force: :cascade do |t|
-    t.date "next_appointment", null: false
-    t.float "weight", null: false
+    t.date "next_appointment"
+    t.float "weight"
     t.string "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "patient_id", null: false
-    t.bigint "diet_id", null: false
+    t.bigint "diet_id"
     t.bigint "appointment_id", null: false
     t.index ["appointment_id"], name: "index_cards_on_appointment_id"
     t.index ["diet_id"], name: "index_cards_on_diet_id"
