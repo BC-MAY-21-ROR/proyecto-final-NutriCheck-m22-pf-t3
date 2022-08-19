a3021bf6c81974948e0ed3d26e7e6bb7d824a8ab
AgustinPalmaM <palma.agustin7@gmail.com>
2022-08-18 20:50:04 -0400

    CReate header - sidebar for desktop views
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,3 +1,11 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+
+
+var allBtns = document.querySelector('.toggle-sidebar-btn');
+allBtns.addEventListener('click', function () {
+  document.querySelector('body').classList.toggle('toggle-sidebar');
+  console.log('hola');
+})
+
